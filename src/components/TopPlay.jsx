189918a38c6f62484950486759f24fc91a1c1ae5
zabuja <atanu.tmsl@gmail.com>
2022-10-17 23:27:55 +0530

    top charts and artists with the swiper implemented

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,101 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+
+// as we need to scroll up every time we dont set the dependency array to empty
+
+const TopChartCard = ({ song, i }) => (
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+    <h3 className="font-bold text-base text-white mr-3">{i+1}</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img  className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title}/>
+    </div>
+  </div>
 );
+const TopPlay = ({ song, i }) => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  const topPlays = data?.slice(0, 5);
+
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: "smooth" });
+  });
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div
+      ref={divRef}
+      className="xl:ml-6 ml-0 zl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col"
+    >
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+
+        <div className=" text-white mt-4 flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard key={song.key} song={song} i  ={i} />
+          ))}
+        </div>
+        <div className="w-full flex flex-col mt-8">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="text-white font-bold text-2xl">Top Artists</h2>
+            <Link to="/top-artists">
+              <p className="text-gray-300 text-base cursor-pointer">See more</p>
+            </Link>
+          </div>
+          <Swiper
+            slidesPerView="auto"
+            spaceBetween={15}
+            freeMode
+            centeredSlides
+            centeredSlidesBounds
+            modules={[FreeMode]}
+            className="mt-4"
+          >
+            {topPlays?.slice(0, 5).map((artist) => (
+              <SwiperSlide
+                key={artist?.key}
+                style={{ width: "25%", height: "auto" }}
+                className="shadow-lg rounded-full animate-slideright"
+              >
+                <Link to={`/artists/${artist?.artists[0].adamid}`}>
+                  <img
+                    src={artist?.images?.background}
+                    alt="Name"
+                    className="rounded-full w-full object-cover"
+                  />
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
